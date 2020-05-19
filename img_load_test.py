1fb1435879ccb13613999e8e6973bf9d04dc5686
nivedk <nivedk1998@gmail.com>
2020-05-19 15:58:29 +0530

    Modified image paths to add drive source

--- img_load_test.py
+++ img_load_test.py
@@ -12,5 +12,3 @@ a2 = a[:,x:,:]
 cv2.imwrite("a1.png", a1)
 cv2.imwrite("a2.png", a2)
 
-for i in range (1,600):
-    print("/"+str(i)+".jpg")
---
+++ path_generator.py
@@ -0,0 +1,2 @@
+for i in range (1,601):
+    print("/content/drive/My Drive/Colab Notebooks/IDCGAN_Github/Single-Image-De-Raining-Keras/dataset/rain/training/"+str(i)+".jpg")
